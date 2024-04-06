34dbe05d05f00c15de6ef7c084b6b30acb6943c8
Andrew Saydashev <www.vfdrey@gmail.com>
2024-04-06 16:00:00 +0400

    renaming by pep

--- lab_2/main.py
+++ lab_2/main.py
@@ -1,16 +1,16 @@
 import os
 
-import Nist_test
+import nist_test
 import json_loader
 
 if __name__ == "__main__":
     java_seq = json_loader.load(os.path.join("lab_2", "source.json"))['java']
     cpp_seq = json_loader.load(os.path.join("lab_2", "source.json"))['cpp']
     print("Nist java test:")
-    print(Nist_test.frequency_bit_test(java_seq))
-    print(Nist_test.identical_consecutive_bits(java_seq))
-    print(Nist_test.longest_sequence_of_ones_test(java_seq))
+    print(nist_test.frequency_bit_test(java_seq))
+    print(nist_test.identical_consecutive_bits(java_seq))
+    print(nist_test.longest_sequence_of_ones_test(java_seq))
     print("Nist cpp test:")
-    print(Nist_test.frequency_bit_test(cpp_seq))
-    print(Nist_test.identical_consecutive_bits(cpp_seq))
-    print(Nist_test.longest_sequence_of_ones_test(cpp_seq))
+    print(nist_test.frequency_bit_test(cpp_seq))
+    print(nist_test.identical_consecutive_bits(cpp_seq))
+    print(nist_test.longest_sequence_of_ones_test(cpp_seq))
